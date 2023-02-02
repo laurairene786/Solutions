e49f8b6530295488420cac431ca95262c535371a
Laura Irene Capo Hernandez <100710546+laurairene786@users.noreply.github.com>
2023-02-02 13:37:16 -0500

    Add files via upload

---
+++ Challenge1.py
@@ -0,0 +1,29 @@
+#Problem description
+#Find out if there are any duplicate urls used in the
+#json placeholder photo data
+
+import json
+from webbrowser import get
+import requests
+
+url = 'https://jsonplaceholder.typicode.com/photos'
+
+#get the data about the photos
+response = requests.get(url)
+
+#read that data into a variable  
+json_data= response.json
+    
+
+#create a list for storing the url of each photo
+url_list = []
+for photo in json_data:
+    #add the url for each photo to the url_list
+    url_list.append(photo['url'])
+
+#How many items are in the url list (Should be 5000 since we have 5000 photos in our dataset)?
+
+print(len(url_list))
+#How many items are there if we turn that list into a set?
+
+
